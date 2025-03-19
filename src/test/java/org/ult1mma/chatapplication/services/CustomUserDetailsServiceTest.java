3b900f86dc6817ab9ef56ed770ca997b5d526ee6
ult1ma <sidorenko3025@gmail.com>
2025-03-20 02:15:36 +0300

    CustomUserDetailsServiceTest.testLoadUserByUsername_Success was added

--- src/test/java/org/ult1mma/chatapplication/services/CustomUserDetailsServiceTest.java
+++ src/test/java/org/ult1mma/chatapplication/services/CustomUserDetailsServiceTest.java
@@ -1,14 +1,19 @@
 package org.ult1mma.chatapplication.services;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.ult1mma.chatapplication.entities.User;
 import org.ult1mma.chatapplication.repositories.UserRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class CustomUserDetailsServiceTest {
@@ -25,6 +30,18 @@ public class CustomUserDetailsServiceTest {
         testUser = new User();
         testUser.setUsername("testUser");
         testUser.setPassword("password");
+    }
+
+    @Test
+    void testLoadUserByUsername_Success() {
+        Mockito.when(userRepository.findByUsername(testUser.getUsername())).thenReturn(testUser);
+
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername(testUser.getUsername());
+        assertNotNull(userDetails);
+        assertEquals("testUser", userDetails.getUsername());
+        assertEquals("password", userDetails.getPassword());
+        assertTrue(userDetails.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("USER")));
+
     }
 
 }
